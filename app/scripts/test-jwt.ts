78d36fdb04b4ef40e420171c18bb1fe3efc4c80d
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-11-22 17:35:54 -0300

    chore: check jwt before inputs gen

--- app/scripts/test-jwt.ts
+++ app/scripts/test-jwt.ts
@@ -1,11 +1,22 @@
-// scripts/test-utils.ts
-import { generateInputs } from 'noir-jwt';
 import { JWT, KEYS } from './fixtures';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+import { generateInputs } from 'noir-jwt';
+import * as jose from 'jose';
 
-async function testFlow() {
+async function testJWTVerification() {
+	console.log('Testing JWT verification...\n');
+	const key = await jose.importJWK(KEYS[0]);
+	const jwtVerif = await jose.jwtVerify(JWT, key);
+
+	console.log(JSON.stringify(jwtVerif, null, 2));
+
 	const result = await generateInputs({ jwt: JWT, pubkey: KEYS[0], maxSignedDataLength: 900 });
-	console.log(result);
 
+	const outputPath = join(process.cwd(), 'scripts', 'result.json');
+	writeFileSync(outputPath, JSON.stringify(result, null, 2));
+
+	console.log(`Results written to: ${outputPath}`);
 }
 
-testFlow();
+testJWTVerification();
